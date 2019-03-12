6d1aed9b381da98199db4c24f8b915bc102eed63
Najites <najitesit@gmail.com>
2019-03-12 22:44:45 +0000

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,9 +1,9 @@
-resource "google_storage_bucket_acl" "default" {
-  count       = "${length(var.role_entity) > 0 ? length(google_storage_bucket.default.*.name) : 0}"
-  default_acl = "${var.default_acl}"
-  bucket      = "${element(google_storage_bucket.default.*.name, count.index)}"
+resource "google_storage_bucket" "default" {
+  name     = "default"
+  location = "US"
 
-  role_entity = [
-    "${var.role_entity}",
-  ]
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
 }
