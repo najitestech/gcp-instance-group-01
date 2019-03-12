eb24c9731e4da903a57b8c884e15a7a55a202099
Najites <najitesit@gmail.com>
2019-03-12 21:46:31 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   credentials = "${file("terraform-project-2020.json")}"
   project     = "terraform-project-2020"
-  region      = "us-east1-b"
+  region      = "${var.region}"
 }
