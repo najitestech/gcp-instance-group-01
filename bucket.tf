f5adac9fe7b095e1866b726e451e73ef07794faf
Najites <najitesit@gmail.com>
2019-03-13 05:40:41 +0000

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "terraform-bucket-2020" {
   name     = "${var.bucket}"
   location = "US"
+  force_destroy = true
 
   website {
     main_page_suffix = "index.html"
