7b762fbd9bd824faf52467c6a3c85fd1397d73a4
Najites <najitesit@gmail.com>
2019-03-13 05:43:42 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "gcs" {
-    bucket  = "terraform-bucket-2020"
-    prefix  = "terraform/state"
-    credentials = "terraform-project-2020.json"
-    project = "my-awesome-project"
-    region  = "us-east1"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    bucket  = "terraform-bucket-2020"
+#    prefix  = "terraform/state"
+#    credentials = "terraform-project-2020.json"
+#    project = "my-awesome-project"
+#    region  = "us-east1"
+#  }
+#}
