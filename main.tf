50793a8c69e08628b514a8483a8d98966539aee2
Najites <najitesit@gmail.com>
2019-03-14 04:02:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,13 @@ module "gci_test" {
 
 module "gce-ilb" {
   source         = "github.com/matti/terraform-google-compute-instance"
+  
   region         = "${var.region}"
   name           = "group2-ilb"
   ports          = ["${module.gci_test.service_port}"]
   health_port    = "${module.gci_test.service_port}"
   source_tags    = ["${module.gci_test.target_tags}"]
-  target_tags    = ["${module.gci_test.target_tags}" ##,"${module.mig3.target_tags}"]
+  target_tags    = ["${module.gci_test.target_tags}" #,"${module.mig3.target_tags}"]
 # backends       = "${module.gci_test.instance_group}"
 #   { group = "${module.gci_test.instance_group}" },
 #    { group = "${module.mig3.instance_group}" },
