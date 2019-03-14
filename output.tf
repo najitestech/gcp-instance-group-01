3cefeb6b742b44e7c7601fea3e15fd79e613a973
Najites <najitesit@gmail.com>
2019-03-14 02:46:29 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "external_ip" {
- value = "module.gci_test.network_interface.0.access_config.0.assigned_nat_ip"
+ value = "${module.gci_test.network_interface.0.access_config.0.assigned_nat_ip}"
 }
 
 #output "this_launch_configuration_id" {
