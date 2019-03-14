451d91e76f24784d6719a3259c696f36760b8d9b
Najites <najitesit@gmail.com>
2019-03-14 03:37:44 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "ip_address" {
- value = "${module.gci_test.External_IP_id}"
-}
+#output "ip_address" {
+# value = "${module.gci_test.External_IP_id}"
+#}
 
 #output "this_launch_configuration_id" {
 #  description = "The ID of the launch configuration"
