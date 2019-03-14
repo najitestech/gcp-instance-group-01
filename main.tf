18e91f1b56e9eb745a3ff812db5f06623aea9a5f
Najites <najitesit@gmail.com>
2019-03-14 05:47:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,13 @@ module "gci_test" {
   user_data    = "${var.user_data}"
 }
 
-module "gci_test2" {
-  source = "github.com/matti/terraform-google-compute-instance"
-
-  amount       = "${var.counts}"
-  region       = "${var.region}"
-  name_prefix  = "${var.gcp-group-name2}"
-  machine_type = "${var.machine_type}"
-  disk_size    = "${var.disk_size}"
-  disk_image   = "${var.disk_image}"
-  user_data    = "${var.user_data}"
-}
+#module "gci_test2" {
+#  source = "github.com/matti/terraform-google-compute-instance"
+#  amount       = "${var.counts}"
+#  region       = "${var.region}"
+#  name_prefix  = "${var.gcp-group-name2}"
+#  machine_type = "${var.machine_type}"
+#  disk_size    = "${var.disk_size}"
+#  disk_image   = "${var.disk_image}"
+#  user_data    = "${var.user_data}"
+#}
