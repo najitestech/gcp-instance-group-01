dca3da9f83cd94408a0d9a11209502b89dc2b9dd
Najites <najitesit@gmail.com>
2019-03-14 03:37:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,16 +10,16 @@ module "gci_test" {
   user_data    = "${var.user_data}"
 }
 
-#module "gce-ilb" {
-#  source         = "github.com/matti/terraform-google-compute-instance"
-#  region         = "${var.region}"
-#  name           = "group2-ilb"
-#  ports          = ["${module.mig2.service_port}"]
-#  health_port    = "${module.mig2.service_port}"
-#  source_tags    = ["${module.mig1.target_tags}"]
-#  target_tags    = ["${module.mig2.target_tags}","${module.mig3.target_tags}"]
-#  backends       = [
-#    { group = "${module.mig2.instance_group}" },
+module "gce-ilb" {
+  source         = "github.com/matti/terraform-google-compute-instance"
+  region         = "${var.region}"
+  name           = "group2-ilb"
+  ports          = ["${module.gci_test.service_port}"]
+  health_port    = "${module.gci_test.service_port}"
+  source_tags    = ["${module.gci_test.target_tags}"]
+  target_tags    = ["${module.gci_test.target_tags}" ##,"${module.mig3.target_tags}"]
+  backends       = [
+    { group = "${module.gci_test.instance_group}" },
 #    { group = "${module.mig3.instance_group}" },
-#  ]
-#}
+  ]
+}
