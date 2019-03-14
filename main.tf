aae0376d6b28ee2105ccbb847443e0cf00af38aa
Najites <najitesit@gmail.com>
2019-03-14 06:37:54 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,35 @@ module "gci_test" {
   disk_size    = "${var.disk_size}"
   disk_image   = "${var.disk_image}"
   user_data    = "${var.user_data}"
-  startup_script = "yum install -y vim"
+}
+
+resource "google_compute_instance" "Centos" {
+   count = "1"
+   name         = "web-server"
+   machine_type = "${var.machine_type}"
+   zone         = "us-east1-c"
+
+   boot_disk {
+     initialize_params {
+     image = "centos-7"
+    }
+}
+
+network_interface {
+   network = "default"
+   access_config {}
+    // Ephemeral IP
+}
+
+
+metadata {
+    ssh_authorized_keys = "${var.ssh_public_key_file}"
+}
+
+    #metadata_startup_script = "yum update && yum install -y httpd && yum innstall -y figlet"
+metadata_startup_script = "${file("${path.module}/centos_scripts.sh")}"
+
+service_account {
+  scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
 }
