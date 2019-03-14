0aa35752dd269ea56906d93b03f88c4f7f6d29a7
Najites <najitesit@gmail.com>
2019-03-14 02:20:09 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "externalip" {
- value = "google_compute_instance.gci_test.*.ip"
+ value = "google_compute_instance.gci_test.*.ip.0.assigned_nat_ip"
 }
 
 #output "this_launch_configuration_id" {
