92bc309a11e996a8681bcc7c68da7677c426b26a
Najites <najitesit@gmail.com>
2019-03-15 04:50:53 +0000

    Add new file

---
+++ centos-script.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/terraform%40terraform-project-2020.iam.gserviceaccount.com"
+sudo yum update -y
+sudo yum -y install -y wget
+sudo yum install -y figlet
+sudo yum install -y httpd
+cat <<EOF > /var/www/html/index.html
+<html><body><h1>Hello World</h1>
+<p>This page was created from a simple startup script!</p>
+</body></html>
+EOF
+
+       #Intalling Ansible
+sudo yum -y install epel-release
+sudo yum -y install ansible
+       #Making some directories
+mkdir /etc/ansible/inventory
+mkdir /etc/ansible/myroles
+       #Adding user
+sudo useradd -p $(openssl passwd -1 Passwd@) najite
