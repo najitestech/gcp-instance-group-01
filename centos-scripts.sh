4ec2ec4c78e5c6f20f85a6e82b731f60a274ec9b
Najites <najitesit@gmail.com>
2019-03-15 05:41:06 +0000

    Update centos-scripts.sh

--- centos-scripts.sh
+++ centos-scripts.sh
@@ -2,10 +2,15 @@
 "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/terraform%40terraform-project-2020.iam.gserviceaccount.com"
 sudo yum update -y
 sudo yum -y install -y wget
-wget http://pkgs.repoforge.org/figlet/figlet-2.2.2-1.el6.rf.x86_64.rpm
+wget ftp://ftp.figlet.org/pub/figlet/program/unix/figlet-2.2.3.tar.gz
+tar -xzvf figlet-2.2.3.tar.gz
+make figlet
+cd figlet-2.2.3
+cp * /usr/local/bin/
 sudo yum install epel-release*
 sudo yum install -y figlet
 sudo yum install -y httpd
+sudo yum update -y
 cat <<EOF > /var/www/html/index.html
 <html><body><h1>Hello World</h1>
 <p>This page was created from a simple startup script!</p>
