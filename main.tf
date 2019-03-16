7df1ae46cdedd6fa804699dd8671fce0697fa284
Najites <najitesit@gmail.com>
2019-03-16 18:46:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,3 +35,15 @@ service_account {
   scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 }
+
+module "app-server" {
+  source = "github.com/matti/terraform-google-compute-instance"
+
+  amount       = "${var.counts}"
+  region       = "${var.region}"
+  name_prefix  = "${var.gcp-group-name}"
+  machine_type = "${var.machine_type}"
+  disk_size    = "${var.disk_size}"
+  disk_image   = "${var.disk_image}"
+  user_data    = "${var.user_data}"
+}
