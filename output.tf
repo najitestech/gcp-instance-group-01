971344f4bb508dee79579ae559ede19d80d3aaac
Najites <najitesit@gmail.com>
2019-03-16 01:17:28 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@
 #}
            #${google_compute_forwarding_rule.default.ip_address
 
-output "Centos" {
-  description = "The ID of the launch configuration"
-  value       = "${google_compute_instance.Centos.ip_address}"
-}
+#output "Centos" {
+#  description = "The ID of the launch configuration"
+#  value       = "${google_compute_instance.Centos.ip_address}"
+#}
