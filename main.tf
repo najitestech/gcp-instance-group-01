84cdc90015f13aa120c3fc3667dae57cc2ba7b46
Najites <najitesit@gmail.com>
2019-03-17 01:17:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-module "gci_test" {
-  source = "github.com/matti/terraform-google-compute-instance"
+#module "gci_test" {
+#  source = "github.com/matti/terraform-google-compute-instance"
 
-  amount       = "${var.counts}"
-  region       = "${var.region}"
-  name_prefix  = "${var.gcp-group-name}"
-  machine_type = "${var.machine_type}"
-  disk_size    = "${var.disk_size}"
-  disk_image   = "${var.disk_image}"
-  user_data    = "${var.user_data}"
-}
+#  amount       = "${var.counts}"
+#  region       = "${var.region}"
+#  name_prefix  = "${var.gcp-group-name}"
+#  machine_type = "${var.machine_type}"
+#  disk_size    = "${var.disk_size}"
+#  disk_image   = "${var.disk_image}"
+#  user_data    = "${var.user_data}"
+#}
 
 resource "google_compute_instance" "Centos" {
-   count = "1"
+   count = "4"
    name         = "web-server"
    machine_type = "${var.machine_type}"
    zone         = "${var.zone}"
