ba1da3f92b9de810b758f39afb79f9dfe61fa651
Najites <najitesit@gmail.com>
2019-03-17 01:51:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "gci_test" {
   source = "github.com/matti/terraform-google-compute-instance"
 
-  amount       = "${var.counts}
+  amount       = "${var.counts}"
   region       = "${var.region}"
   name_prefix  = "${var.gcp-group-name}${count.index + 1}"
   machine_type = "${var.machine_type}"
