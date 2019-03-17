23deda4bc65ed3c1f0fae78a9feba17ec4098ee7
Najites <najitesit@gmail.com>
2019-03-17 00:22:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+module "mig1" {
+  source            = "GoogleCloudPlatform/managed-instance-group/google"
+  version           = "1.1.14"
+  region            = "${var.region}"
+  zone              = "${var.zone}"
+  name              = "group1"
+  size              = 2
+  service_port      = 80
+  service_port_name = "http"
+  http_health_check = false
+  target_pools      = ["${module.gci_test.target_pool}"]
+  target_tags       = ["allow-service1"]
+  ssh_source_ranges = ["0.0.0.0/0"]
+
 module "gci_test" {
   source = "github.com/matti/terraform-google-compute-instance"
 
